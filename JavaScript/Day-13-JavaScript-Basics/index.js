727ec615607a0ffc2a807eeb9a9c1c382d36808c
Suraj <121674047+Suraj-Singh07@users.noreply.github.com>
2026-07-29 21:49:28 +0530

    Add JavaScript basics examples

---
+++ JavaScript/Day-13-JavaScript-Basics/index.js
@@ -0,0 +1,8 @@
+console.log("Hey there!");
+console.log("Hello World!");
+console.log(123);
+console.log(true);
+console.log(null);
+console.log(undefined);
+console.log(typeof (6));
+console.log(typeof ("abd"));
