2df08a388c3a89101ade7ae9968355cb3ec5d2ae
Suraj <121674047+Suraj-Singh07@users.noreply.github.com>
2026-07-31 23:46:58 +0530

    Add JavaScript variables and data types examples

---
+++ JavaScript/Day-14-JavaScript-Variables/index.js
@@ -0,0 +1,24 @@
+console.log([25, 'Harsh', true, null]);
+console.log({
+    hello: 248,
+    name: 'Aryan',
+    pass: 'Aryan11'
+});
+
+x = 50;
+console.log(x);
+x = 100;
+console.log(x);
+
+var name = 'Vishal';
+name = 'Sagar';
+console.log(name);
+
+let age = 18;
+age = 22;
+let age = 'twenty-five'
+console.log(age);
+
+const address = 'Pune';
+address = 'Nashik';
+console.log(address);
