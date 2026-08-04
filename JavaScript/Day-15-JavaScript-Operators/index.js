495769e311e898f0512d110f8882f4e717dc3bd5
Suraj <121674047+Suraj-Singh07@users.noreply.github.com>
2026-08-04 23:52:11 +0530

    Learn JavaScript operators and build GST price calculator

---
+++ JavaScript/Day-15-JavaScript-Operators/index.js
@@ -0,0 +1,58 @@
+// let a, b , c = 2, 4, 9;
+// console.log(b);
+
+// a = 3;
+// b = 5;
+// c = 8;
+// b = 6;
+// a = 4;
+// console.log(a);
+
+// Arithmetic operator
+let a, b;
+a = 20;
+b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(5 ** 4);
+
+a = 56;
+console.log(a++);
+console.log(--b);
+
+
+let score = 340;
+let total_Score = 750;
+let per = (score / total_Score) * 100;
+console.log(per);
+
+let product = 580;
+let gst = (product * 18) / 100;
+// console.log("18% gst on product is " + gst);
+let final_Price = product + gst;
+console.log(final_Price);
+
+
+// Assignment Operator
+let x = 35;
+console.log(x += 2);
+console.log(x -= 8);
+
+// Comparison Operator
+
+let y = 5;
+let z = 6;
+let h = 'a';
+
+console.log(y > z);
+console.log(y < z);
+console.log(y >= z);
+console.log(y <= z);
+console.log(y == h);
+console.log(y === h);
+console.log(y != h);
+console.log(y !== h);
